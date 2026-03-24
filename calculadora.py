e13da084228c0b6df69972149704922318d2452a
Breno Dias <brdfigueira@gmail.com>
2026-03-24 14:06:36 -0300

    Repositório aula python

---
+++ calculadora.py
@@ -0,0 +1,13 @@
+"""
+ Receber dois valores numéricos do usuário
+ e apresentar o resultado das operações matemáticas 
+ básicas entre eles: soma, subtração, multiplicação, 
+ divisão, resto da divisão e exponenciação
+"""
+
+numero1 = float(input("Digite o primeiro número: "))
+numero2 = float(input("Digite o segundo número: "))
+
+print(numero1,"+",numero2,"=",(numero1+numero2))
+print(f"{numero1}+{numero2}={numero1+numero2}")
+
